85c4c5a64e2291a177d272862f36d7492fd8ad1c
Prajakta <85434875+prajakta2405@users.noreply.github.com>
2023-07-25 12:50:02 +0530

    Added Solution - GfG to GitHub

---
+++ School/Remove vowels from string/remove-vowels-from-string.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+class Solution:
+	def removeVowels(self, S):
+		# code here
+		res=""
+		vowels=['a','e','i','o','u','A','E','I','O','U']
+		for i in S:
+		    if i not in vowels:
+		        res+=i
+		        
+	    return res
+		        
+		        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		s = input()
+		
+		ob = Solution()	
+		answer = ob.removeVowels(s)
+		
+		print(answer)
+
+
+# } Driver Code Ends

0c5e854cd36e4a329cb175f20a1499e42e35f7bd
Prajakta <85434875+prajakta2405@users.noreply.github.com>
2023-07-31 11:05:03 +0530

    Added Solution - GfG to GitHub

---
+++ School/Sum of AP series/sum-of-ap-series.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+	def sum_of_ap(self, n, a, d):
+		# Code here
+		sum1=n/2*(2*a+(n-1)*d)
+		return int(sum1)
+		
+		
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n, a, d = input().split()
+		n = int(n)
+		a = int(a)
+		d = int(d)
+		ob = Solution();
+		ans = ob.sum_of_ap(n, a, d)
+		print(ans)
+# } Driver Code Ends

d2a4ee1a75fc3ac1ae8f3c20142cde403a9cd588
Prajakta <85434875+prajakta2405@users.noreply.github.com>
2023-10-10 14:59:06 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Binary number to decimal number/binary-number-to-decimal-number.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+class Solution:
+	def binary_to_decimal(self, str):
+	    num=0
+	    for i in str:
+	        num=num*2+int(i)
+	    return num
+		# Code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		str = input()
+		ob = Solution();
+		ans = ob.binary_to_decimal(str)
+		print(ans)
+# } Driver Code Ends
